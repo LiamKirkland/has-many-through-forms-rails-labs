7dfe6f2392257a2f74ea02cdd8f35b6d8ba2bf35
LiamKirkland <liam.b.kirkland@gmail.com>
2026-08-11 13:04:44 -0400

    Has Many Through Forms

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attr)
+    username = user_attr[:username]
+    return if username.blank?
+
+    self.user = User.find_or_create_by(username: username)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ApplicationRecord
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
+  def categories_attributes=(categories_attrs)
+    categories_attrs.each_value do |attrs|
+      name = attrs[:name]
+      next if name.blank?
 
+      category = Category.find_or_create_by(name: name)
+      categories << category unless categories.include?(category)
+    end
+  end
 end
